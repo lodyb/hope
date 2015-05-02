82990e283e8655df50a31083525172005b128bb5
ami <ami@lilah.cat>
2015-05-02 22:25:42 +0100

    added more phaser code, included dev resources from a prior project for testing

--- hope.js
+++ hope.js
@@ -2,64 +2,78 @@
 /**
  * globals
  */
-var game = null;
-var input = null;
+game = null;
+input = null;
 
 /**
- * main game object
+ * main game object class
  */
-var Game = {
-
-	player: null,
+var HopeGame = {
 
 	init: function() {
-		input = Object.create(Input);
-
-		/**
-		 * configure input
-		 */
-		input.init(this, document, [
-			{name: 'up', code: [38, 87],
-				preventDefault: true, callback: null},
-			{name: 'down', code: [40, 83],
-				preventDefault: true, callback: null},
-			{name: 'left', code: [37, 65],
-				preventDefault: true, callback: null},
-			{name: 'right', code: [39, 68],
-				preventDefault: true, callback: null},
-		]);
-
-		/**
-		 * init phaser
-		 */
-		game = new Phaser.Game(640, 360, Phaser.AUTO, 'main', {
-			preload: this.preload, create: this.create});
+		this.player = null;
 	},
 
 	/**
 	 * preload resources
 	 */
 	preload: function() {
-		game.load.image('logo', 'res/test.png');
+		debug('preloading resources');
+		game.load.image('sprites/space_bkg', 'res/sprites/space_bg.png');
+		game.load.image('tilesheets/dev', 'res/tilesheets/dev.png');
+		game.load.spritesheet('spritesheets/player_dev',
+			'res/spritesheets/player_dev.png');
+		game.load.tilemap('maps/test', 'res/maps/test.json', null,
+			Phaser.Tilemap.TILED_JSON);
 	},
 
 	/**
 	 * resources loaded - init game
 	 */
 	create: function() {
+		debug('preloaded resources', 'success');
+		debug('starting game');
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 		this.player = Object.create(Player);
 		this.player.init();
-		var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-		logo.anchor.setTo(0.5, 0.5);
 	},
 };
 
 /**
- * resources loaded - init game
+ * init game objects
  */
 window.onload = function() {
 	debug('hope started', 'success');
-	var game = Object.create(Game);
-	game.init();
+
+	/**
+	 * configure input
+	 */
+	input = Object.create(Input);
+	input.init(this, document, [
+		{name: 'up', code: [38, 87],
+			preventDefault: true, callback: null},
+		{name: 'down', code: [40, 83],
+			preventDefault: true, callback: null},
+		{name: 'left', code: [37, 65],
+			preventDefault: true, callback: null},
+		{name: 'right', code: [39, 68],
+			preventDefault: true, callback: null},
+	]);
+
+
+	/**
+	 * init phaser
+	 */
+	game = new Phaser.Game(640, 360, Phaser.AUTO, 'main');
+
+	/**
+	 * game states
+	 */
+	game.state.add('main', HopeGame);
+
+	/**
+	 * init state chain
+	 */
+	game.state.start('main');
 };
 
